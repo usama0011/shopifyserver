ce4aa54464889dbf10686304b9f0ccd8e2e1500a
usama0011 <za5232208@gmail.com>
2024-03-15 15:22:35 +0500

    Update User Code

--- auth/authroute.js
+++ auth/authroute.js
@@ -2,14 +2,13 @@ import express from "express";
 import {
   LoginUser,
   RegisteredUser,
-  verifyUser,Logout
+  Logout,
 } from "../controllers/authcontroller.js";
 
 const router = express.Router();
 
 router.post("/register", RegisteredUser);
 router.post("/login", LoginUser);
-router.post("/verifyemail", verifyUser);
-router.get("/logout",Logout );
+router.get("/logout", Logout);
 
 export default router;
--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,29 +1,20 @@
 import User from "../models/usermodel.js";
 import bcrypt from "bcrypt";
-import nodemailer from "nodemailer";
 import jwt from "jsonwebtoken";
-const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: "za5232208@gmail.com",
-    pass: "vyte whll dnyg pxlo",
-  },
-});
 
 export const RegisteredUser = async (req, res) => {
   try {
     const { fullname, email, phonenumber, password, invitationcode } = req.body;
 
     // Check if the user already exists
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ phonenumber });
     if (existingUser) {
       return res
         .status(400)
-        .json({ message: "User already exists with this email" });
+        .json({ message: "User already exists with this Phone number" });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
     // Generate a 6-digit verification code
-    const verificationCode = Math.floor(100000 + Math.random() * 900000);
 
     // Save verification code to the database
     const newUser = new User({
@@ -32,24 +23,8 @@ export const RegisteredUser = async (req, res) => {
       phonenumber,
       password: hashedPassword,
       invitationcode,
-      verificationCode,
     });
     await newUser.save();
-
-    // Send verification email
-    const mailOptions = {
-      from: "za5232208@gmail.com",
-      to: email,
-      subject: "Email Verification",
-      text: `Your verification code is: ${verificationCode}`,
-    };
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error("Error sending email:", error);
-      } else {
-        console.log("Email sent:", info.response);
-      }
-    });
 
     res.status(201).json({ message: "User Registered Successfully" });
   } catch (error) {
@@ -117,40 +92,10 @@ export const LoginUser = async (req, res) => {
   }
 };
 
-export const verifyUser = async (req, res) => {
-  try {
-    const { verificationCode } = req.body;
-    console.log(verificationCode);
-    // Find the user by email
-    const user = await User.findOne({ verificationCode });
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    // Check if the verification code matches
-    if (user.verificationCode !== verificationCode) {
-      return res.status(400).json({ message: "Invalid verification code" });
-    }
-
-    // Update user's verification status
-    user.isVerified = true;
-    await user.save();
-
-    res.status(200).json({ message: "User verified successfully" });
-  } catch (error) {
-    console.error("Error verifying user:", error);
-    res
-      .status(500)
-      .json({ message: "Internal Server Error", errmsg: error.message });
-  }
-};
-
 export const Logout = async (req, res) => {
   try {
     // Clear the "lInfo" cookie
     res.clearCookie("lInfo");
-
     // You can also send a response indicating successful logout
     res.status(200).json({ message: "Logout successful" });
   } catch (error) {
